4cd1efa84e25346914566466b5ef46ada57208ad
rafaelZangado <fael.andrade0@gmail.com>
2023-07-22 12:24:03 -0300

    :recycle: ajustando o view blanck

--- resources/views/blanck.blade.php
+++ resources/views/blanck.blade.php
@@ -11,16 +11,16 @@
 
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo mr-5" href="index.html">
-              {{-- MINHA LOGO
-              <img src="images/logo.svg" class="mr-2" alt="logo" /> --}}
+            <a class="navbar-brand brand-logo mr-5">
+              {{-- MINHA LOGO --}}
+              <img src="images/logo.svg" class="mr-2" alt="logo" />
             </a>
-            <a class="navbar-brand brand-logo-mini" href="index.html">
-              {{-- MINHA OUTRA LOGO
-              <img src="images/logo-mini.svg" alt="logo" /></a> --}}
+            <a class="navbar-brand brand-logo-mini" >
+              {{-- MINHA OUTRA LOGO --}}
+              <img src="images/logo-mini.svg" alt="logo" /></a>
         </div>
-        {{-- <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+        <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        {{-- <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
           <span class="icon-menu"></span>
         </button>
         <ul class="navbar-nav mr-lg-2">
@@ -34,9 +34,9 @@
               <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
             </div>
           </li>
-        </ul>    
-       
-      </div> --}}
+        </ul> --}}
+
+      </div>
     </nav>
 
     <div class="container-scroller">
