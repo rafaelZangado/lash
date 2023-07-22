43d8728054ce030063108f4c2f0c53eeec52ffcd
rafaelZangado <fael.andrade0@gmail.com>
2023-07-22 12:40:43 -0300

    :recycle: modificando o campo que tem a rota

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,15 +1,15 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
 crossorigin="anonymous">
 </script>
 
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  
+
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
   <ul class="nav" id="menu">
-    
+
     <li class="nav-item">
-      <a class="nav-link" href="{{route('dashboard')}}" >
+      <a class="nav-link" href="{{route('atendimento')}}" >
          <i class="icon-grid menu-icon"></i>
          <span class="material-icons"></span>
         <span class="menu-title">Dashboard</span>
@@ -17,8 +17,8 @@ crossorigin="anonymous">
     </li>
 
     <li class="nav-item">
-      <a id="link-agendamento" class="nav-link" href="{{route('agendamento')}}" aria-controls="ui-basic" >    {{-- style="background: #4B49AC" --}}     
-        <i class="mdi mdi-calendar-clock menu-icon" ></i>{{--style="color: #fff"--}}  
+      <a id="link-agendamento" class="nav-link" href="{{route('agendamento')}}" aria-controls="ui-basic" >    {{-- style="background: #4B49AC" --}}
+        <i class="mdi mdi-calendar-clock menu-icon" ></i>{{--style="color: #fff"--}}
         <span class="menu-title" >{{--style="color: #fff"--}}
         Agendamento
         </span>
@@ -30,21 +30,21 @@ crossorigin="anonymous">
         <i class="icon-columns menu-icon"></i>
         <span class="menu-title">
           Registrar procedimento
-        </span>       
+        </span>
       </a>
     </li>
 
     <li class="nav-item">
-      <a id="link-clientes" class="nav-link"  href="{{route('clientes')}}" 
+      <a id="link-clientes" class="nav-link"  href="{{route('clientes')}}"
       aria-expanded="false" aria-controls="auth">
         <i class="icon-head menu-icon"></i>
-        <i class="fas fa-shield-alt"></i>       
+        <i class="fas fa-shield-alt"></i>
         <span class="menu-title">
           Meus clientes
-        </span>        
+        </span>
       </a>
     </li>
- 
+
     <li class="nav-item">
       <a class="nav-link" onclick="loadContent('views/anamnese.php')">
         <i class="icon-paper menu-icon"></i>
@@ -58,7 +58,7 @@ crossorigin="anonymous">
         <span class="menu-title">Atendimento</span>
       </a>
     </li>
-    
+
   </ul>
 </nav>
 
