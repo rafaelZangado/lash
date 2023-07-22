5629a5261bf32a3741e92558d234e4233835afac
rafaelZangado <fael.andrade0@gmail.com>
2023-07-22 16:56:44 -0300

    :sparkles: criação da mascara para moeda

--- public/js/mask.js
+++ public/js/mask.js
@@ -15,27 +15,44 @@
     });
 
 //Mascara para Fone
+    function mask(o, f) {
+        setTimeout(function() {
+        var v = mphone(o.value);
+        if (v != o.value) {
+            o.value = v;
+        }
+        }, 1);
+    }
 
-function mask(o, f) {
-    setTimeout(function() {
-      var v = mphone(o.value);
-      if (v != o.value) {
-        o.value = v;
-      }
-    }, 1);
-  }
-
-  function mphone(v) {
+    function mphone(v) {
     var r = v.replace(/\D/g, "");
     r = r.replace(/^0/, "");
     if (r.length > 10) {
-      r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+        r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
     } else if (r.length > 5) {
-      r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+        r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
     } else if (r.length > 2) {
-      r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+        r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
     } else {
-      r = r.replace(/^(\d*)/, "($1");
+        r = r.replace(/^(\d*)/, "($1");
     }
     return r;
+    }
+
+//Mascara para Moeda
+
+    function moeda(){
+        var elemento = document.getElementById('valor');
+  var valor = elemento.value;
+
+  valor = valor + '';
+  valor = parseInt(valor.replace(/[\D]+/g,''));
+  valor = valor + '';
+  valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+  if (valor.length > 6) {
+    valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
   }
+
+  elemento.value = valor;
+    }
